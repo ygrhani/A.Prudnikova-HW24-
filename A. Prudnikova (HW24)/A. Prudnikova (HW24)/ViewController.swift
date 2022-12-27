bffc0666b7b8f72d474eb8e495570961ca5b432c
ygrhani <121520744+ygrhani@users.noreply.github.com>
2022-12-28 00:41:44 +0300

    New Project Created

---
+++ A. Prudnikova (HW24)/A. Prudnikova (HW24)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A. Prudnikova (HW24)
+//
+//  Created by Ann Prudnikova on 28.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
